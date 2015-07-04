fb864c5766645816b80b930eac14c786d6bf5ac8
bryguy9312 <bryguys9312@gmail.com>
2015-07-04 01:17:19 -0700

    Added JS for map and markers

--- js/map.js
+++ js/map.js
@@ -1,32 +1,43 @@
+"use strict";
 // Function to draw your map
-var drawMap() = function() {
+var global = {};
 
-  // Create map and set viewd
- 
+var drawMap = function () {
+    global.map = L.map('container').setView([47.6550, -122.3080], 12);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        id: 'bryguy9312.mkcia67o',
+        accessToken: 'pk.eyJ1IjoiYnJ5Z3V5OTMxMiIsImEiOiJhNGI1ZTI1ZTUwNGI0ZjFiZTlkOWM1OTBiMzUwYjAzMSJ9.Wka7nqdGk03XA4pOpTuWgA'
+    }).addTo(global.map);
 
-  // Create an tile layer variable using the appropriate url
+    getData();
 
-  // Add the layer to your map
- 
+};
 
-  // Execute your function to get data
- 
-}
-
-// Function for getting data
-var getData = function() {
-
-  // Execute an AJAX request to get the data in data/response.js
-
-
-  // When your request is successful, call your customBuild function
-
-}
+var data;
+var getData = function () {
+    $.ajax({
+        url: 'data/response.json',
+        type: 'get',
+        success:function(dat) {
+            data = dat;
+            customBuild();
+        },
+        error: function(err) {
+            console.log(err);
+        },
+        dataType: "json"
+    });
+};
 
 // Do something creative with the data here!  
 var customBuild = function() {
-
-  
-}
+    data.map(function(incident) {
+        var marker = new L.circleMarker([incident.lat, incident.lng], {
+            radius: '7',
+            color: 'red'
+        });
+        marker.addTo(global.map);
+    });
+};
 
 
