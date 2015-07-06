70aa6cd312e088d03014c52755a0d1e8b4c3ca6a
bryguy9312 <bryguys9312@gmail.com>
2015-07-06 03:17:24 -0700

    Added styling and features to the map

--- js/map.js
+++ js/map.js
@@ -3,9 +3,9 @@
 var global = {};
 
 var drawMap = function () {
-    global.map = L.map('container').setView([47.6550, -122.3080], 12);
+    global.map = L.map('container').setView([39.8282, -98.5795], 3);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        id: 'bryguy9312.mkcia67o',
+        id: 'bryguy9312.ml24922k',
         accessToken: 'pk.eyJ1IjoiYnJ5Z3V5OTMxMiIsImEiOiJhNGI1ZTI1ZTUwNGI0ZjFiZTlkOWM1OTBiMzUwYjAzMSJ9.Wka7nqdGk03XA4pOpTuWgA'
     }).addTo(global.map);
 
@@ -32,12 +32,43 @@ var getData = function () {
 // Do something creative with the data here!  
 var customBuild = function() {
     data.map(function(incident) {
-        var marker = new L.circleMarker([incident.lat, incident.lng], {
-            radius: '7',
-            color: 'red'
+        var color;
+        var aIcon;
+        if (incident["Hit or Killed?"] == 'Hit') {
+            color = 'blue';
+        } else {
+            color = 'red';
+        }
+
+        if (incident["Victim's Gender"] == "Male") {
+            aIcon = 'mars';
+        } else {
+            aIcon = 'venus';
+        }
+
+        var awesomeMarker = L.AwesomeMarkers.icon({
+            icon: aIcon,
+            markerColor: color
         });
-        marker.addTo(global.map);
+        var marker = L.marker([incident.lat, incident.lng], {icon: awesomeMarker});
+        var summ = "Victim Name: "
+                    + incident['Victim Name']
+                    + "<br>"
+                    +"Race: "
+                    +incident["Race"]
+                    +"<br>"
+                    + " Summary: "
+                    + incident.Summary
+                    +"<br>"
+                    +"<a href="+incident["Source Link"]+">Source</a>";
+        if (incident.lng < -20 && incident['Victim Name'] != "Alejandro Pinedo" && incident['Victim Name'] != "David Jerome Maestas") {
+            marker.bindPopup(summ);
+            marker.addTo(global.map);
+        }
     });
 };
+
+
+
 
 
